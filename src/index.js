4350184254a2695bd46e78c7ae7757442ecee3c4
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-06-23 22:56:59 -0600

    Completed Section 3.36: Passing Props - Solutions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import Faker from 'faker';
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div className='ui container comments'>
+      <CommentDetail
+        author={Faker.internet.userName()}
+        time={Faker.date.weekday()}
+        text={Faker.hacker.phrase()}
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author={Faker.internet.userName()}
+        time={Faker.date.weekday()}
+        text={Faker.hacker.phrase()}
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author={Faker.internet.userName()}
+        time={Faker.date.weekday()}
+        text={Faker.hacker.phrase()}
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author={Faker.internet.userName()}
+        time={Faker.date.weekday()}
+        text={Faker.hacker.phrase()}
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author={Faker.internet.userName()}
+        time={Faker.date.weekday()}
+        text={Faker.hacker.phrase()}
+        avatar={Faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
